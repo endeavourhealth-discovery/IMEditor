a581a261efe9d3acf9a22c2e84136c0c5ae40cc7
matt-voror <matt@voror.co.uk>
2022-06-21 15:50:41 +0100

    excluded im-library from Deps in vite config. Added node/java combined server settings also

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,34 @@ import { esbuildCommonjs } from "@originjs/vite-plugin-commonjs";
 export default defineConfig({
   base: "./",
   plugins: [vue()],
-  optimizeDeps: {
-    esbuildOptions: {
-      plugins: [esbuildCommonjs(["google-palette"])]
-    }
-  },
   resolve: {
     dedupe: ["vue"],
     alias: { "@": path.resolve(__dirname, "./src"), "./runtimeConfig": "./runtimeConfig.browser" }
+  },
+  server: {
+    port: 8082,
+    proxy: {
+      "/imapi": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: p => p.replace(/^\/imapi/, "")
+      },
+      "/nodeapi": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+        rewrite: p => p.replace(/^\/nodeapi/, "")
+      }
+    }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [esbuildCommonjs(["google-palette"])]
+    },
+    exclude: ["im-library"]
   },
   test: {
     globals: true,
