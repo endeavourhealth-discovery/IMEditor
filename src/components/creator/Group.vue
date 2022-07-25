f21d347706bd1f71eb0f46811037472c01c27c65
matt-voror <matt@voror.co.uk>
2022-07-25 17:47:04 +0100

    moved dynamic components into separate script and set to use componentType enum for component selection

--- src/components/creator/Group.vue
+++ src/components/creator/Group.vue
@@ -6,20 +6,27 @@
   </div>
 </template>
 
-<script setup lang="ts">
-import { PropertyGroup, PropertyShape, TTIriRef } from "im-library/dist/types/interfaces/Interfaces";
-import { ref, Ref, computed, watch, inject, onMounted, PropType } from "vue";
+<script lang="ts">
 import EntityMultiSearch from "@/components/shapeComponents/EntityMultiSearch.vue";
 import EntityMultiDropdown from "../shapeComponents/EntityMultiDropdown.vue";
 import EntitySingleDropdown from "../shapeComponents/EntitySingleDropdown.vue";
 import HtmlSingleSelect from "@/components/shapeComponents/HtmlSingleSelect.vue";
 import StringSingleSelect from "@/components/shapeComponents/StringSingleSelect.vue";
 import StringSingleDisplay from "@/components/shapeComponents/StringSingleDisplay.vue";
+
+export default defineComponent({
+  components: { EntityMultiDropdown, EntityMultiSearch, EntitySingleDropdown, HtmlSingleSelect, StringSingleDisplay, StringSingleSelect }
+});
+</script>
+
+<script setup lang="ts">
+import { PropertyGroup, PropertyShape, TTIriRef } from "im-library/dist/types/interfaces/Interfaces";
+import { ref, Ref, computed, watch, inject, onMounted, PropType, defineComponent } from "vue";
 import { Enums, Helpers, Services, Vocabulary } from "im-library";
 import injectionKeys from "@/injectionKeys/injectionKeys";
 import axios from "axios";
 import _ from "lodash";
-const { EditorMode } = Enums;
+const { ComponentType, EditorMode } = Enums;
 const { IM, RDF, RDFS } = Vocabulary;
 const {
   DataTypeCheckers: { isObjectHasKeys }
@@ -33,7 +40,7 @@ const props = defineProps({
 watch(
   () => props.data,
   async () => {
-    properties.value = props.data.property;
+    if (props.data && props.data.property) properties.value = props.data.property;
   }
 );
 
@@ -44,17 +51,17 @@ let properties: Ref<PropertyShape[]> = ref([...props.data.property]);
 function processComponentType(type: TTIriRef): any {
   switch (type["@id"]) {
     case IM.LABEL_COMPONENT:
-      return StringSingleDisplay;
+      return ComponentType.STRING_SINGLE_DISPLAY;
     case IM.TEXT_INPUT_COMPONENT:
-      return StringSingleSelect;
+      return ComponentType.STRING_SINGLE_SELECT;
     case IM.HTML_EDITOR_COMPONENT:
-      return HtmlSingleSelect;
+      return ComponentType.HTML_SINGLE_SELECT;
     case IM.ENTITY_MULTI_SEARCH_COMPONENT:
-      return EntityMultiSearch;
+      return ComponentType.ENTITY_MULTI_SEARCH;
     case IM.ENTITY_MULTI_DROPDOWN_COMPONENT:
-      return EntityMultiDropdown;
+      return ComponentType.ENTITY_MULTI_DROPDOWN;
     case IM.ENTITY_SINGLE_DROPDOWN_COMPONENT:
-      return EntitySingleDropdown;
+      return ComponentType.ENTITY_SINGLE_DROPDOWN;
     default:
       throw new Error("Invalid component type encountered in shape group");
   }
