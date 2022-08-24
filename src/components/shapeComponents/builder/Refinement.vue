8534fb4d3f087c668d67cbcd02eb1fe8ac49aa46
matt-voror <matt@voror.co.uk>
2022-08-24 11:54:26 +0100

    swapped to composition

--- src/components/shapeComponents/builder/Refinement.vue
+++ src/components/shapeComponents/builder/Refinement.vue
@@ -13,8 +13,9 @@
           :value="child.value"
           :id="child.id"
           :position="child.position"
-          :builderType="child.builderType"
+          :shape="child.shape"
           :showButtons="child.showButtons"
+          :mode="child.mode"
           @deleteClicked="deleteItem"
           @addClicked="addItemWrapper"
           @updateClicked="updateItemWrapper"
@@ -36,208 +37,229 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "@vue/runtime-core";
-import { mapState } from "vuex";
-import AddDeleteButtons from "@/components/edit/memberEditor/builder/AddDeleteButtons.vue";
 import Property from "@/components/edit/memberEditor/builder/Property.vue";
 import Quantifier from "@/components/edit/memberEditor/builder/Quantifier.vue";
 import AddNext from "@/components/edit/memberEditor/builder/AddNext.vue";
+
+export default defineComponent({
+  components: { Property, Quantifier, AddNext }
+});
+</script>
+
+<script setup lang="ts">
+import { defineComponent, PropType, ref, Ref, watch, computed, onMounted, nextTick } from "vue";
+import { mapState, useStore } from "vuex";
+import AddDeleteButtons from "@/components/edit/memberEditor/builder/AddDeleteButtons.vue";
+import _ from "lodash";
 import { Vocabulary, Helpers, Enums } from "im-library";
-import { EntityReferenceNode, NextComponentSummary, ComponentDetails, TTIriRef } from "im-library/dist/types/interfaces/Interfaces";
+import {
+  EntityReferenceNode,
+  NextComponentSummary,
+  ComponentDetails,
+  TTIriRef,
+  PropertyShape,
+  PropertyGroup
+} from "im-library/dist/types/interfaces/Interfaces";
 const {
   EditorBuilderJsonMethods: { generateNewComponent, updateItem, addItem, addNextOptions, scrollIntoView, updatePositions }
 } = Helpers;
 const { RDFS, RDF } = Vocabulary;
 const { ComponentType } = Enums;
 
-export default defineComponent({
-  name: "Refinement",
-  props: {
-    id: { type: String, required: true },
-    position: { type: Number, required: true },
-    value: { type: Object as PropType<{ propertyIri: string; children: any[]; associatedMember: TTIriRef | undefined }>, required: false },
-    showButtons: { type: Object as PropType<{ minus: boolean; plus: boolean }>, default: { minus: true, plus: true } },
-    builderType: { type: String as PropType<Enums.BuilderType>, required: true }
-  },
-  emits: {
-    updateClicked: (_payload: ComponentDetails) => true,
-    addNextOptionsClicked: (_payload: any) => true,
-    deleteClicked: (_payload: ComponentDetails) => true,
-    addClicked: (_payload: any) => true
-  },
-  components: { AddDeleteButtons, Property, Quantifier, AddNext },
-  watch: {
-    refinementBuild: {
-      handler() {
-        if (!this.loading) this.onConfirm();
+const props = defineProps({
+  id: { type: String, required: true },
+  position: { type: Number, required: true },
+  value: { type: Object as PropType<{ propertyIri: string; children: any[]; associatedMember: TTIriRef | undefined }>, required: false },
+  showButtons: { type: Object as PropType<{ minus: boolean; plus: boolean }>, default: { minus: true, plus: true } },
+  shape: { type: Object as PropType<PropertyShape | PropertyGroup>, required: true },
+  mode: { type: String as PropType<Enums.EditorMode>, required: true }
+});
+
+const emit = defineEmits({
+  updateClicked: (_payload: ComponentDetails) => true,
+  addNextOptionsClicked: (_payload: any) => true,
+  deleteClicked: (_payload: ComponentDetails) => true,
+  addClicked: (_payload: any) => true
+});
+
+const store = useStore();
+const filterOptions = computed(() => store.state.filterOptions);
+
+onMounted(async () => {
+  await createBuild();
+});
+
+let refinementBuild: Ref<ComponentDetails[]> = ref([]);
+let loading = ref(true);
+
+watch(refinementBuild, () => {
+  if (!loading.value) onConfirm();
+});
+
+async function createBuild() {
+  loading.value = true;
+  refinementBuild.value = [];
+  if (!hasData(props.value)) createDefaultBuild();
+  else {
+    let position = 0;
+    const property = generateNewComponent(
+      ComponentType.PROPERTY,
+      position,
+      {
+        propertyIri: props.value.propertyIri,
+        associatedMember: props.value.associatedMember
       },
-      deep: true
+      props.shape,
+      { minus: false, plus: false },
+      props.mode
+    );
+    if (property) {
+      refinementBuild.value.push(property);
+      position++;
     }
-  },
-  computed: mapState(["filterOptions", "selectedFilters"]),
-  async mounted() {
-    await this.createBuild();
-  },
-  data() {
-    return {
-      refinementBuild: [] as ComponentDetails[],
-      loading: true
-    };
-  },
-  methods: {
-    async createBuild() {
-      this.loading = true;
-      this.refinementBuild = [];
-      if (!this.hasData(this.value)) this.createDefaultBuild();
-      else {
-        let position = 0;
-        const property = generateNewComponent(
-          ComponentType.PROPERTY,
-          position,
-          {
-            propertyIri: this.value.propertyIri,
-            associatedMember: this.value.associatedMember
-          },
-          this.builderType,
-          { minus: false, plus: false }
-        );
-        if (property) {
-          this.refinementBuild.push(property);
-          position++;
-        }
 
-        for (const child of this.value.children) {
-          const quantifier = generateNewComponent(
-            ComponentType.QUANTIFIER,
-            position,
-            { propertyIri: this.value.propertyIri, quantifier: child },
-            this.builderType,
-            { minus: false, plus: false }
-          );
-          if (quantifier) {
-            this.refinementBuild.push(quantifier);
-            position++;
-          }
-        }
+    for (const child of props.value.children) {
+      const quantifier = generateNewComponent(
+        ComponentType.QUANTIFIER,
+        position,
+        { propertyIri: props.value.propertyIri, quantifier: child },
+        props.shape,
+        { minus: false, plus: false },
+        props.mode
+      );
+      if (quantifier) {
+        refinementBuild.value.push(quantifier);
+        position++;
       }
-      this.loading = false;
-    },
-
-    createDefaultBuild() {
-      this.refinementBuild = [];
-      const property = generateNewComponent(ComponentType.PROPERTY, 0, { propertyIri: "", associatedMember: {} }, this.builderType, {
-        minus: false,
-        plus: false
-      });
-      if (property) this.refinementBuild.push(property);
-      const quantifier = generateNewComponent(ComponentType.QUANTIFIER, 1, { propertyIri: "", quantifier: {} }, this.builderType, {
-        minus: false,
-        plus: false
-      });
-      if (quantifier) this.refinementBuild.push(quantifier);
-    },
-
-    hasData(data: any): data is { propertyIri: string; children: any[]; associatedMember: TTIriRef } {
-      if (data && (data as { propertyIri: string; children: any[]; associatedMember: TTIriRef }).propertyIri) return true;
-      return false;
-    },
-
-    deleteItem(data: ComponentDetails): void {
-      const index = this.refinementBuild.findIndex(item => item.position === data.position);
-      this.refinementBuild.splice(index, 1);
-      const length = this.refinementBuild.length;
-      if (length === 0) {
-        this.createDefaultBuild();
-        return;
-      }
-      if (this.refinementBuild[0].type !== ComponentType.PROPERTY) {
-        const property = generateNewComponent(
-          ComponentType.PROPERTY,
-          0,
-          { propertyIri: this.value?.propertyIri || "", associatedMember: this.value?.associatedMember || {} },
-          this.builderType,
-          { minus: false, plus: false }
-        );
-        if (property) this.refinementBuild.unshift(property);
-      }
-      updatePositions(this.refinementBuild);
-    },
-
-    updateItemWrapper(data: ComponentDetails) {
-      updateItem(data, this.refinementBuild);
-    },
-
-    async addNextOptionsWrapper(data: NextComponentSummary): Promise<void> {
-      const nextOptionsComponent = addNextOptions(data, this.refinementBuild, this.builderType);
-      await this.$nextTick();
-      scrollIntoView(nextOptionsComponent);
-    },
-
-    addItemWrapper(data: { selectedType: Enums.ComponentType; position: number; value: any }): void {
-      if (data.selectedType === ComponentType.PROPERTY) {
-        data.value = { propertyIri: this.value?.propertyIri, associatedMember: this.value?.associatedMember };
-      }
-      addItem(data, this.refinementBuild, this.builderType, { minus: false, plus: false });
-    },
-
-    onConfirm() {
-      this.$emit("updateClicked", {
-        id: this.id,
-        value: this.createAsValue(),
-        position: this.position,
-        type: ComponentType.REFINEMENT,
-        builderType: this.builderType,
-        json: this.createAsJson(),
-        showButtons: this.showButtons
-      });
-    },
-
-    deleteClicked(): void {
-      this.$emit("deleteClicked", {
-        id: this.id,
-        value: this.createAsValue(),
-        position: this.position,
-        type: ComponentType.REFINEMENT,
-        builderType: this.builderType,
-        json: this.createAsJson(),
-        showButtons: this.showButtons
-      });
-    },
-
-    createAsJson() {
-      let json = {} as any;
-      let propertyIri = "";
-      let children = [] as any[];
-      for (const [index, item] of this.refinementBuild.entries()) {
-        if (index === 0) propertyIri = item.value.propertyIri ? item.value.propertyIri : "";
-        else if (item.type !== ComponentType.ADD_NEXT) children.push(item.json);
-      }
-      json[propertyIri] = children;
-      return json;
-    },
-
-    createAsValue() {
-      const children = [];
-      let propertyIri = "";
-      for (const [index, child] of this.refinementBuild.entries()) {
-        if (index === 0) propertyIri = child.value.propertyIri ? child.value.propertyIri : "";
-        else children.push(child.value);
-      }
-      return { propertyIri: propertyIri, children: children, associatedMember: this.value?.associatedMember };
-    },
-
-    addNextClicked(item: any): void {
-      this.$emit("addNextOptionsClicked", {
-        position: this.position + 1,
-        selectedType: item
-      });
-    },
-
-    getButtonOptions() {
-      return [ComponentType.ENTITY, ComponentType.LOGIC, ComponentType.REFINEMENT];
     }
   }
-});
+  loading.value = false;
+}
+
+function createDefaultBuild() {
+  refinementBuild.value = [];
+  const property = generateNewComponent(
+    ComponentType.PROPERTY,
+    0,
+    { propertyIri: "", associatedMember: {} },
+    props.shape,
+    { minus: false, plus: false },
+    props.mode
+  );
+  if (property) refinementBuild.value.push(property);
+  const quantifier = generateNewComponent(
+    ComponentType.QUANTIFIER,
+    1,
+    { propertyIri: "", quantifier: {} },
+    props.shape,
+    { minus: false, plus: false },
+    props.mode
+  );
+  if (quantifier) refinementBuild.value.push(quantifier);
+}
+
+function hasData(data: any): data is { propertyIri: string; children: any[]; associatedMember: TTIriRef } {
+  if (data && (data as { propertyIri: string; children: any[]; associatedMember: TTIriRef }).propertyIri) return true;
+  return false;
+}
+
+function deleteItem(data: ComponentDetails): void {
+  const index = refinementBuild.value.findIndex(item => item.position === data.position);
+  refinementBuild.value.splice(index, 1);
+  const length = refinementBuild.value.length;
+  if (length === 0) {
+    createDefaultBuild();
+    return;
+  }
+  if (refinementBuild.value[0].type !== ComponentType.PROPERTY) {
+    const property = generateNewComponent(
+      ComponentType.PROPERTY,
+      0,
+      { propertyIri: props.value?.propertyIri || "", associatedMember: props.value?.associatedMember || {} },
+      props.shape,
+      { minus: false, plus: false },
+      props.mode
+    );
+    if (property) refinementBuild.value.unshift(property);
+  }
+  updatePositions(refinementBuild.value);
+}
+
+function updateItemWrapper(data: ComponentDetails) {
+  updateItem(data, refinementBuild.value);
+}
+
+async function addNextOptionsWrapper(data: NextComponentSummary): Promise<void> {
+  const nextOptionsComponent = addNextOptions(data, refinementBuild.value, props.shape, props.mode);
+  await nextTick();
+  scrollIntoView(nextOptionsComponent);
+}
+
+function addItemWrapper(data: { selectedType: Enums.ComponentType; position: number; value: any }): void {
+  if (data.selectedType === ComponentType.PROPERTY) {
+    data.value = { propertyIri: props.value?.propertyIri, associatedMember: props.value?.associatedMember };
+  }
+  addItem(data, refinementBuild.value, { minus: false, plus: false }, props.shape, props.mode);
+}
+
+function onConfirm() {
+  emit("updateClicked", {
+    id: props.id,
+    value: createAsValue(),
+    position: props.position,
+    type: ComponentType.REFINEMENT,
+    shape: props.shape,
+    json: createAsJson(),
+    showButtons: props.showButtons,
+    mode: props.mode
+  });
+}
+
+function deleteClicked(): void {
+  emit("deleteClicked", {
+    id: props.id,
+    value: createAsValue(),
+    position: props.position,
+    type: ComponentType.REFINEMENT,
+    shape: props.shape,
+    json: createAsJson(),
+    showButtons: props.showButtons,
+    mode: props.mode
+  });
+}
+
+function createAsJson() {
+  let json = {} as any;
+  let propertyIri = "";
+  let children = [] as any[];
+  for (const [index, item] of refinementBuild.value.entries()) {
+    if (index === 0) propertyIri = item.value.propertyIri ? item.value.propertyIri : "";
+    else if (item.type !== ComponentType.ADD_NEXT) children.push(item.json);
+  }
+  json[propertyIri] = children;
+  return json;
+}
+
+function createAsValue() {
+  const children = [];
+  let propertyIri = "";
+  for (const [index, child] of refinementBuild.value.entries()) {
+    if (index === 0) propertyIri = child.value.propertyIri ? child.value.propertyIri : "";
+    else children.push(child.value);
+  }
+  return { propertyIri: propertyIri, children: children, associatedMember: props.value?.associatedMember };
+}
+
+function addNextClicked(item: any): void {
+  emit("addNextOptionsClicked", {
+    position: props.position + 1,
+    selectedType: item
+  });
+}
+
+function getButtonOptions() {
+  return [ComponentType.ENTITY, ComponentType.LOGIC, ComponentType.REFINEMENT];
+}
 </script>
 
 <style scoped>
