23d3735faa282edb61bbff5bbe36fa455f951323
Kostas Kardatos <kon.kardatos@gmail.com>
2022-11-01 14:11:13 +0200

    add indentation + move add buttons underneath

--- src/components/shapeComponents/SetDefinitionForm.vue
+++ src/components/shapeComponents/SetDefinitionForm.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-for="(included, clauseIndex) of clauses" class="clause-wrapper">
-    <div class="clause-container">
+  <div v-for="(included, clauseIndex) of clauses">
+    <li class="rule-container">
       <Button icon="pi pi-times" class="p-button-rounded p-button-danger p-button-text" @click="removeClause(clauseIndex)" />
       <Dropdown v-model="included.include" :options="includeOptions" option-label="name" option-value="value" placeholder="Include or exclude" />
       <EntityAutocomplete :ttAlias="included.concept" />
@@ -11,42 +11,39 @@
         option-value="value"
         placeholder="Include subtypes"
       />
-      <Button icon="pi pi-cog" v-tooltip="'Add refinement'" class="p-button-warning one-rem-margin" @click="addRefinement(clauseIndex)" />
-      <Button
-        v-if="clauseIndex === clauses.length - 1"
-        icon="pi pi-plus"
-        v-tooltip="'Add concept'"
-        class="p-button-success one-rem-margin"
-        @click="addConcept"
-      />
+    </li>
+
+    <div v-for="(refinement, refinementIndex) of included.refinements">
+      <ul class="refinement-container">
+        <Button icon="pi pi-times" class="p-button-rounded p-button-danger p-button-text" @click="removeRefinement(clauseIndex, refinementIndex)" />
+        <EntityAutocomplete
+          :ttAlias="refinement.property"
+          :parentClauseIri="included.concept['@id']"
+          :get-suggestions-method="getAllowablePropertySuggestions"
+        />
+        <Dropdown
+          v-model="refinement.property.includeSubtypes"
+          :options="includeSubtypesOptions"
+          option-label="name"
+          option-value="value"
+          placeholder="Include subtypes"
+        />
+        <i class="icon pi pi-arrow-right" />
+        <EntityAutocomplete :ttAlias="refinement.is" :parentClauseIri="refinement.property['@id']" :get-suggestions-method="getAllowableRangeSuggestions" />
+        <Dropdown
+          v-model="refinement.is.includeSubtypes"
+          :options="includeSubtypesOptions"
+          option-label="name"
+          option-value="value"
+          placeholder="Include subtypes"
+        />
+      </ul>
     </div>
-    <div v-for="(refinement, refinementIndex) of included.refinements" class="refinement-wrapper">
-      <div class="refinement-container">
-        <div class="clause-container">
-          <Button icon="pi pi-times" class="p-button-rounded p-button-danger p-button-text" @click="removeRefinement(clauseIndex, refinementIndex)" />
-          <EntityAutocomplete
-            :ttAlias="refinement.property"
-            :parentClauseIri="included.concept['@id']"
-            :get-suggestions-method="getAllowablePropertySuggestions"
-          />
-          <Dropdown
-            v-model="refinement.property.includeSubtypes"
-            :options="includeSubtypesOptions"
-            option-label="name"
-            option-value="value"
-            placeholder="Include subtypes"
-          />
-          <i class="icon pi pi-arrow-right" />
-          <EntityAutocomplete :ttAlias="refinement.is" :parentClauseIri="refinement.property['@id']" :get-suggestions-method="getAllowableRangeSuggestions" />
-          <Dropdown
-            v-model="refinement.is.includeSubtypes"
-            :options="includeSubtypesOptions"
-            option-label="name"
-            option-value="value"
-            placeholder="Include subtypes"
-          />
-        </div>
-      </div>
+    <ul class="refinement-button">
+      <Button icon="pi pi-cog" label="Add refinement" class="p-button-warning" @click="addRefinement(clauseIndex)" />
+    </ul>
+    <div class="rule-button">
+      <Button v-if="clauseIndex === clauses.length - 1" icon="pi pi-plus" label="Add rule" class="p-button-success" @click="addConcept" />
     </div>
   </div>
 </template>
@@ -99,24 +96,19 @@ async function getAllowableRangeSuggestions(iri: string, searchTerm: string) {
 </script>
 
 <style scoped>
-.clause-container {
-  display: flex;
-  flex-flow: row nowrap;
-  justify-content: center;
+.rule-container {
+  list-style-type: none;
+  margin: none;
 }
+
 .refinement-container {
-  display: flex;
-  flex-flow: column nowrap;
-  justify-content: center;
-  align-items: center;
+  list-style-type: none;
+  margin: 0rem;
 }
+
 .icon {
   align-self: center;
   padding-left: 1rem;
   padding-right: 1rem;
-}
-
-.clause-wrapper {
-  padding-bottom: 2rem;
 }
 </style>
